b6669d71470e3f777727617aefb2bf53e42eb7b2
blueskinlizard <blueskinlizard@gmail.com>
2025-03-10 15:45:27 -0500

    Made it so create task could not be spammed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import "./styles.css";
 import displayManipulation from "./displayManipulation";
 const newForm = document.getElementById("create-new-button");
 const pageDisplay = new displayManipulation();
+let createTracker = 0;
 document.addEventListener("DOMContentLoaded", ()=>{
 })
     newForm.addEventListener("click", () => {
-        pageDisplay.createInputCard();
+        if(createTracker === 0){
+            createTracker++;
+            pageDisplay.createInputCard();
         const form = document.getElementById("pop-up-box-task-create-form");
         form.addEventListener("submit", (event) => {    
             event.preventDefault(); 
             pageDisplay.buttonSubmission();  
+            createTracker = 0;
         });
+        }
+        
         
     });
 
